7ad0bce5d114f0fa7be48db45f10dcf3cb552a9f
nadavzilber@gmail.com <nadavzilber@gmail.com>
2020-05-19 23:20:08 +0300

    changed to google-speadsheet, connected frontend to sheets with functions

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
 import TasksContainer from "./components/TasksContainer";
-
 function App() {
   return (
     <div className="App">
--- src/components/TasksContainer.js
+++ src/components/TasksContainer.js
@@ -1,15 +1,50 @@
-import React, { useRef, useState } from "react";
+import React, { useRef, useState, useEffect } from "react";
 import Task from "./Task";
+import {
+  authenticate,
+  addNewRow,
+  updateRow,
+  deleteRow,
+  getExistingData,
+} from "./SpreadSheet";
 
 const TasksContainer = () => {
   const newTaskInput = useRef(null);
   const [taskList, setTaskList] = useState([
-    { id: 1, msg: "do this", isChecked: false },
-    { id: 2, msg: "do that", isChecked: false },
-    { id: 3, msg: "and then this", isChecked: false },
-    { id: 4, msg: "then do that", isChecked: false },
-    { id: 5, msg: "and this", isChecked: false },
+    // { id: 1, msg: "do this", isChecked: false },
+    // { id: 2, msg: "do that", isChecked: false },
+    // { id: 3, msg: "and then this", isChecked: false },
+    // { id: 4, msg: "then do that", isChecked: false },
+    // { id: 5, msg: "and this", isChecked: false },
   ]);
+  const [fetchingData, setFetchingData] = useState(true);
+
+  useEffect(() => {
+    console.log("env:::::>", process.env.REACT_APP_GOOGLE_SHEETS_API_KEY);
+    console.log("TODO: move the config to root or to the .env file");
+    console.log("useEffect");
+    const fetchData = async () => {
+      console.log("fetchData");
+      // You can await here
+      //const response = await MyAPI.getData(someId);
+      await authenticate();
+      let existingData = await getExistingData();
+      console.log("existingData ====> ", existingData);
+      let newTaskList = existingData.map((row) => {
+        return {
+          id: row.id,
+          msg: row.msg,
+          isChecked: row.isChecked == "TRUE",
+          date: row.date,
+        };
+      });
+      console.log("newTaskList:", newTaskList);
+      await setTaskList(newTaskList);
+      console.log("UPDATED TASK LIST:", taskList);
+      setFetchingData(false);
+    };
+    fetchData();
+  }, []);
 
   const keyPressHandler = async (e) => {
     let code = e.keyCode ? e.keyCode : e.which;
@@ -27,13 +62,18 @@ const TasksContainer = () => {
 
   const addTask = async () => {
     let tempTasks = [...taskList];
+    console.log("taskList[taskList.length]", taskList[taskList.length - 1]);
     let newTask = {
-      id: taskList[taskList.length - 1].id + 1,
+      id:
+        taskList.length > 0
+          ? parseInt(taskList[taskList.length - 1].id) + 1
+          : 1,
       msg: newTaskInput.current.value,
       isChecked: false,
     };
     tempTasks.push(newTask);
     await setTaskList(tempTasks);
+    await addNewRow(newTask);
   };
 
   const resetInput = () => {
@@ -56,13 +96,16 @@ const TasksContainer = () => {
   };
 
   const onToggleCheck = (taskID) => {
+    let isChecked;
     let tempList = taskList.map((task) => {
       if (task.id === taskID) {
         task.isChecked = !task.isChecked;
+        isChecked = task.isChecked;
       }
       return task;
     });
     setTaskList(tempList);
+    updateRow(taskID, isChecked);
   };
 
   const onTaskDelete = (taskID) => {
@@ -70,24 +113,31 @@ const TasksContainer = () => {
       return task.id !== taskID;
     });
     setTaskList(tempList);
+    deleteRow(taskID);
   };
 
   return (
-    <div className="tasks-container">
-      <div class="tdl-holder">
-        <h2>TO DO LIST</h2>
-        <div class="tdl-content">
-          <ul>{renderTasks()}</ul>
+    <>
+      {fetchingData ? (
+        <h4>Fetching Data</h4>
+      ) : (
+        <div className="tasks-container">
+          <div className="tdl-holder">
+            <h2>TO DO LIST</h2>
+            <div className="tdl-content">
+              <ul>{renderTasks()}</ul>
+            </div>
+            <input
+              type="text"
+              className="tdl-new"
+              onKeyDown={(e) => keyPressHandler(e)}
+              placeholder="Write new task and hit 'Enter'..."
+              ref={newTaskInput}
+            />
+          </div>
         </div>
-        <input
-          type="text"
-          class="tdl-new"
-          onKeyDown={(e) => keyPressHandler(e)}
-          placeholder="Write new task and hit 'Enter'..."
-          ref={newTaskInput}
-        />
-      </div>
-    </div>
+      )}
+    </>
   );
 };
 
